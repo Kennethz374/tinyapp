972b2ebd67c160ec2f67da56192d9e88a7bcd2de
vagrant <Kennethzhang374@gmail.com>
2019-09-03 02:34:23 +0000

    finished all day 1 request

--- express_server.js
+++ express_server.js
@@ -29,14 +29,21 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.redirect(`/urls/${generateRandomString()}`);         // Respond with redirect
+  let s = generateRandomString();
+  urlDatabase[s]= req.body.longURL;  // Log the POST request body to the console
+  res.redirect(`/urls/${s}`);         // Respond with redirect
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
